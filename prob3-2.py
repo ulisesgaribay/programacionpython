c1ec083fd738532000374ef92fa312daf4344a31
ulisesgaribay <ulesex21101996@gmail.com>
2016-11-30 17:55:29 -0600

    problema 2 lista de ejercicios 3

---
+++ prob3-2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+print "ingresa un numero de 4 digitos"
+x = [(raw_input())]
+a = int(x[0][0])
+b = int(x[0][1])
+c = int(x[0][2])
+d = int(x[0][3])
+print "la suma de tu numero digito por digito es:", a+b+c+d
+
