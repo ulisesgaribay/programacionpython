18645ef269889dc1d0d4492b31e08f67df3300e6
ulisesgaribay <ulesex21101996@gmail.com>
2016-12-01 01:58:51 -0600

    problema 7 lista de ejercicios 3

---
+++ prob3-7.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+print "ingresa un nombre corto de entre 3 y 6 letras en minusculas"
+x = [raw_input()]
+[m]=x
+n = len(m)
+if n == 6:
+	y = x[0][0]
+	a = x[0][1]
+	b = x[0][2]
+	c = x[0][3]
+	d = x[0][4]
+	e = x[0][5]
+	if x[0][0] == "a":
+	        [x] = x
+	        print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+	elif x[0][0] == "e":
+	        [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+	elif x[0][0] == "i":
+	        [x] = x
+	        print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+	elif x[0][0] == "o":
+	        [x] = x
+	        print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+	elif x[0][0] == "u":
+	        [x] = x
+	        print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+	else:
+	        x = a+b+c+d+e
+	        print y+x,y+x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x,y+x
+
+if n == 5:
+        y = x[0][0]
+        a = x[0][1]
+        b = x[0][2]
+        c = x[0][3]
+        d = x[0][4]
+        if x[0][0] == "a":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "e":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "i":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "o":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "u":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        else:
+                x = a+b+c+d
+                print y+x,y+x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x,y+x
+
+if n == 4:
+	y = x[0][0]
+        a = x[0][1]
+        b = x[0][2]
+        c = x[0][3]
+        if x[0][0] == "a":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "e":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "i":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "o":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "u":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        else:
+                x = a+b+c
+                print y+x,y+x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x,y+x
+
+if n == 3:
+	y = x[0][0]
+        a = x[0][1]
+        b = x[0][2]
+        if x[0][0] == "a":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "e":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "i":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "o":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        elif x[0][0] == "u":
+                [x] = x
+                print x,x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x, x
+        else:
+                x = a+b
+                print y+x,y+x,"bob%s"%x, "banana bana fo fof%s"%x ,"fi fa y fo m%s"%x,y+x
+
